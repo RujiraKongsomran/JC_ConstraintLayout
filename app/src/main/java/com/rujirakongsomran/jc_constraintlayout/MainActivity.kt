fe5567c905f4e0413fcd9273899fbad9fd357245
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-10-30 09:22:21 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/jc_constraintlayout/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_constraintlayout/MainActivity.kt
@@ -3,11 +3,22 @@ package com.rujirakongsomran.jc_constraintlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.rujirakongsomran.jc_constraintlayout.ui.theme.JC_ConstraintLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,12 +26,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JC_ConstraintLayoutTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                ConstraintLayout()
             }
         }
+    }
+}
+
+@Composable
+fun ConstraintLayout() {
+    val constraints = ConstraintSet {
+        val greenBox = createRefFor("greenbox")
+        val redBox = createRefFor("redbox")
+        val guideline = createGuidelineFromTop(0.5f)
+
+        constrain(greenBox) {
+            top.linkTo(guideline)
+            start.linkTo(parent.start)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+
+        constrain(redBox) {
+            top.linkTo(parent.top)
+            start.linkTo(greenBox.end)
+            end.linkTo(parent.end)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+
+        createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
+    }
+
+    ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .layoutId("greenbox")
+        )
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+                .layoutId("redbox")
+        )
     }
 }
 
@@ -33,6 +80,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JC_ConstraintLayoutTheme {
-        Greeting("Android")
+        ConstraintLayout()
     }
 }
